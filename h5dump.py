fecbdf18cf11abe66c5557e123f7de43e0115ce4
Julien Lafaye <jlafaye@gmail.com>
2019-07-09 20:56:07 +0200

    Use numpy array to buffer events
    This should be more efficient memory-wise than storing python objects

--- h5dump.py
+++ h5dump.py
@@ -10,93 +10,132 @@
 import os
 import sys
 
-sys.path.append(os.environ['PERF_EXEC_PATH'] + \
-	'/scripts/python/Perf-Trace-Util/lib/Perf/Trace')
-
 import pandas as pd
 import pandas.io.pytables as pt
-import datetime as dt
 import numpy as np
-import copy
 
-from perf_trace_context import *
-from Core import *
+from perf_trace_context import * # noqa
+from Core import *  # noqa
+import logging
 
-events   = {}
+sys.path.append(os.environ['PERF_EXEC_PATH'] +
+                '/scripts/python/Perf-Trace-Util/lib/Perf/Trace')
+
+events = {}
 filename = None
 
-def mapType(s):
-    """
-    Map a type as returned through perf callback to 
-    a type suitable for a pytables instance
-    Return a pair priority,type. Priority should be
-    used in case a single series contain multiple
-    different types
-    """
-    if s == int:
-        return 1, np.dtype('<i8')
-    elif s == long:
-        return 2, np.dtype('<u8')
-    elif s == type(str('')):
-        return 3, np.dtype('|S16')
-    else:
-        return 0, s
+logger = logging.getLogger('h5dump')
+logging.basicConfig(level=logging.INFO)
+
+
+def getDtype(value):
+    if isinstance(value, int):
+        return np.dtype('<i8')
+    elif isinstance(value, str):
+        return np.dtype('|S16')
+    return None
+
+
+class Vector:
+
+    def __init__(self, dtype):
+        self._size = 0
+        self._storage_size = 1
+        self._storage = np.zeros(self._storage_size,
+                                 dtype=dtype)
+
+    def size(self):
+        return self._size
+
+    def data(self):
+        return self._storage[:self._storage_size]
+
+    def resize(self, new_storage_size=None):
+        if new_storage_size is None:
+            new_storage_size = 2*self._storage_size
+        new_storage = np.zeros(new_storage_size,
+                               dtype=self._storage.dtype)
+
+        # copy existing data
+        if new_storage_size > self._storage_size:
+            new_storage[:self._storage_size] = self._storage
+        else:
+            new_storage = self._storage[:new_storage_size]
+
+        self._storage_size = new_storage_size
+        self._storage = new_storage
+
+    def push_back(self, item):
+        if self._size >= self._storage_size:
+            self.resize()
+        self._storage[self._size] = item
+        self._size += 1
+
 
 def trace_begin():
     global filename
-    # print "+++ in trace_begin"
-    import sys
     if len(sys.argv) < 2:
-        print("(EE) missing filename")
+        logger.error("missing filename")
         sys.exit(1)
     filename = sys.argv[1]
 
+
 def trace_end():
-    print("+++ opening file '%s'" % filename)
+
+    logger.info('opening output file {}'.format(filename))
     store = pt.HDFStore(filename)
-    for event_name in events:
-        print("+++ adding group for event '%s': %d entry(ies) found" \
-            % (event_name, len(events[event_name])))
 
-        # enrich data with a timestamp that will
-        # be used as index. We don't use python
-        # datetime & timedelta instances as they
-        # are not natively supported by PyTables
-        first = True
-        types = set()
-        index = []
-        for s in events[event_name]:
-            timestamp = s['common_s'] + float(s['common_ns'])/1000
-            index.append(timestamp)
+    for event_name, vd in events.items():
 
-        # build type mapping
-        # to avoid PerformanceWarning becaused of mixed types (int/long)
-        # or generic objects
-        df = pd.DataFrame(events[event_name], index=index)
-        types = {}
-        for col in df:
-            types[col] = set(map(type, df[col])) 
-        for col, srcTypes in types.iteritems():
-            dstTypes = map(mapType, srcTypes)
-            dstTypes.sort()
-            dstType = dstTypes[-1][1] # dstType should be the one with the highest priority
+        logger.info("writing data for event '{}'".format(event_name))
 
-            # change type if necessary
-            if df[col].dtype != dstType:
-                print("   %s: changing col '%s' type %s->%s" % (event_name, col, str(df[col].dtype), str(dstType)))
-                df[col] = df[col].astype(dstType) 
+        ad = {field: v.data() for field, v in vd.items()}
 
-        print("+++ storing dataframe, event:%s size:%d columns:%s" % (event_name, df.shape[0], ",".join(df.columns)))
-        print(df.head())
+        secs = ad['common_s']
+        nsecs = ad['common_ns']
+        timestamp = secs.astype(np.float) + nsecs.astype(np.float) * 1E-9
+
+        df = pd.DataFrame(ad,
+                          index=timestamp)
+
         store[event_name] = df
+
     store.close()
-
-        
-
-def trace_unhandled(event_name, context, event_fields_dict):
-    events.setdefault(event_name, []).append(copy.deepcopy(event_fields_dict))
+    logger.info('done')
 
 
-# if __name__ == '__main__':
-#print('This script is supposed to be loaded by perf and and run directly')
-#    print('Usage: perf script -s h5dump.py <filename>')
+def event_to_vector_dict(event):
+    vector_dict = {}
+    for field, value in event.items():
+        dtype = getDtype(value)
+        if dtype is None:
+            logger.warning("ignoring field '{}' with type {}"
+                           .format(field, type(value)))
+            continue
+        logger.info("detected column '{}' with dtype {}".format(field, dtype))
+        vector = Vector(dtype)
+        vector.push_back(value)
+        vector_dict[field] = vector
+    return vector_dict
+
+
+def trace_unhandled(event_name, context, event_fields_dict, perf_sample_dict):
+    global events
+    if event_name not in events:
+        vector_dict = event_to_vector_dict(event_fields_dict)
+        events[event_name] = vector_dict
+
+    vector_dict = events[event_name]
+    for field, vector in vector_dict.items():
+        value = event_fields_dict[field]
+        vector.push_back(value)
+
+
+def print_header(event_name, cpu, secs, nsecs, pid, comm):
+    print("%-20s %5u %05u.%09u %8u %-20s " %
+          (event_name, cpu, secs, nsecs, pid, comm), end="")
+
+
+def get_dict_as_string(a_dict, delimiter=' '):
+    return delimiter.join(['%s=%s' % (k, str(v))
+                          for k, v in sorted(a_dict.items())])
